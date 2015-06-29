7e8a65e5b1174e308a5c74ca476f291254392796
Kolesnikov Alexander <alexander.kolesnikov@teknavo.com>
2015-06-29 14:15:18 +0300

    Added login tests

--- spec/acceptance_tests/account_management/login_feature_spec.rb
+++ spec/acceptance_tests/account_management/login_feature_spec.rb
@@ -1,23 +1,37 @@
 require "rails_helper"
 require 'spec_helper'
 
-RSpec.feature 'user_management.user_login', :type => :feature do
 
+RSpec.feature 'user_management.user_login', :type => :feature do
+  background do
+	visit "/"
+	Account.create(email: 'test1@user.com', password: '123456qwe' subdomain: 'MySuperSD')
+	Account.create(email: 'test2@user.com', password: '123456asd' subdomain: 'MySuperSD1')
+  end
 
   scenario "User visits login page" do
-    visit "/"
-
 	find_field('E-mail').visible?
 	find_field('Password').visible?
 	find_button("Log in")
   end
 
    scenario 'user logs in with correct creds' do
-	visit '/'
 	fill_in('E-mail', with: 'test@user.com')
 	fill_in('Password', with: '123456qwe')
 	click_button "Log in"
+	expect(current_path).to eq('/admin')
+	expect(page).to have_content('Hello test@user.com')
+	expect(page).to have_content('Subdomain MySuperSD')
    end
+
+   scenario 'user logs in with incorrect login' do
+	fill_in('E-mail', with: 'test@user.com')
+	fill_in('Password', with: '123456qwe!!!')
+	click_button "Log in"
+	expect(current_path).to eq('/')
+	expect(page.find('#errors')).to have_content('Incorrect email/password')
+   end
+
 
 #   scenario 'user logs in with correct creds' do
 #   	visit '/'
