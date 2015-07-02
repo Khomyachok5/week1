2864e054e513569c1924df965e4d4eb8ec834a5f
Kolesnikov Alexander <alexander.kolesnikov@teknavo.com>
2015-07-02 17:38:56 +0300

    refactored tests added scenarios for user following re-set instructions

--- spec/acceptance_tests/account_management/login_feature_spec.rb
+++ spec/acceptance_tests/account_management/login_feature_spec.rb
@@ -15,49 +15,49 @@ RSpec.feature 'user_management.user_login', :type => :feature do
 	find_button("Log in")
   end
 
-   scenario 'user logs in with correct creds' do
+  scenario 'user logs in with correct creds' do
 	fill_in('E-mail', with: 'test1@user.com')
 	fill_in('Password', with: '123456qwe')
 	click_button "Log in"
 	expect_page_url_to_be '/admin'
 	expect(page).to have_content('Hello test1@user.com')
 	expect(page).to have_content('Subdomain MySuperSD')
-   end
+  end
 
-   scenario 'user logs in with incorrect login' do
+  scenario 'user logs in with incorrect login' do
 	fill_in('E-mail', with: 'test@user.com')
 	fill_in('Password', with: '123456qwe')
 	click_button "Log in"
 	expect_page_url_to_be '/'
 	expect(page.find('#errors')).to have_content('Incorrect email/password')
 	find_link('Re-set password').visible?
-   end
+  end
 
-   scenario 'user opens password reset page' do
+  scenario 'user opens password reset page' do
 	click_link "Re-set password"
 	expect_page_url_to_be '/forgotpassword'
 	find_field('E-mail').visible?
 	find_button("Email instructions", disabled: true)
-   end
+  end
 
-   scenario 'user enters valid email to re-set password form' do
+  scenario 'user enters valid email to re-set password form' do
 	click_link "Re-set password"
 	expect_page_url_to_be '/forgotpassword'
 	find_field('E-mail').visible?
 	find_button("Email instructions", disabled: true)
 	fill_in('E-mail', with: 'test1@user.com')
 	find_button("Email instructions")
-   end
+  end
 
-   scenario 'user sends password re-set instructions' do
+  scenario 'user sends password re-set instructions' do
 	click_link "Re-set password"
 	expect_page_url_to_be '/forgotpassword'
 	fill_in('E-mail', with: 'test1@user.com')
 	click_button("Email instructions")
 	expect(page).to have_content('instructions were sent')
-   end
+  end
 
-   scenario 'user sends password re-set instructions to non-existing email' do
+  scenario 'user sends password re-set instructions to non-existing email' do
 	click_link "Re-set password"
 	expect_page_url_to_be '/forgotpassword'
 	fill_in('E-mail', with: 'test1@user.ru')
@@ -66,31 +66,56 @@ RSpec.feature 'user_management.user_login', :type => :feature do
 	find_field('E-mail').visible?
 	find_button("Email instructions", disabled: true)
 	expect(page).to have_content("Couldn't find account for test1@user.ru'")
-   end
+  end
 
-   scenario 'user receives password re-set instructions' do
-	click_link "Re-set password"
-	expect_page_url_to_be '/forgotpassword'
-	fill_in('E-mail', with: 'test1@user.com')
-	click_button("Email instructions")
+  context "User has sent re-set instructions email" do
+	before(:each) do
+		click_link "Re-set password"
+		expect_page_url_to_be '/forgotpassword'
+		fill_in('E-mail', with: 'test1@user.com')
+		click_button("Email instructions")
+	end
+	after(:each) do
+		reset_mailer
+	end
+	scenario 'user receives password re-set instructions' do
+		expect(unread_emails_for("test1@user.com").size).to eql 1
+		open_last_email_for("test1@user.com")
+		expect(current_email).to have_subject(/Password re-set instructions for Week1/)
+		expect(current_email).to have_body_text(/Follow the link to re-set your password/)
+	end
 
-    expect(unread_emails_for("test1@user.com").size).to eql 1
-    open_last_email_for("test1@user.com")
-    expect(current_email).to have_subject(/Password re-set instructions for Week1/)
-    expect(current_email).to have_body_text(/Follow the link to re-set your password/)
-   end
+	scenario 'password re-set instructions contain link to password re-set page' do
+		open_last_email_for("test1@user.com")
+		url = /href=\"([^"]*)\"/.match(current_email.body.to_s)[1]
+		expect(url.length).to be > 1
+	end
 
-   scenario 'password re-set instructions contain link to password re-set page' do
-	click_link "Re-set password"
-	expect_page_url_to_be '/forgotpassword'
-	fill_in('E-mail', with: 'test1@user.com')
-	click_button("Email instructions")
+	scenario 'user follows password re-set link from letter' do
+		open_last_email_for("test1@user.com")
+		url = /href=\"([^"]*)\"/.match(current_email.body.to_s)[1]
+		visit url
+		find_field('Password').visible?
+		find_field('Password confirmation').visible?
+		find_button("Set password", disabled: true)
+	end
 
-    open_last_email_for("test1@user.com")
-    url = /href=\"([^"]*)\"/.match(current_email.body.to_s)[1]
-    expect(url.length).to be > 1
-   end
-
+  context "User has received re-set instructions email and followed password re-set link from letter" do
+	before(:each) do
+		open_last_email_for("test1@user.com")
+		url = /href=\"([^"]*)\"/.match(current_email.body.to_s)[1]
+		visit url
+		find_field('Password').visible?
+		find_field('Password confirmation').visible?
+		find_button("Set password", disabled: true)
+	end
+	scenario 'user sets new password' do
+		fill_in('Password', with: '123456qwe_new')
+		fill_in('Password confirmation', with: '123456qwe_new')
+		click_button("Set password")
+	end
+  end
+  end
 
 	def expect_page_url_to_be(url)
 		expect(current_path).to eq(url)
