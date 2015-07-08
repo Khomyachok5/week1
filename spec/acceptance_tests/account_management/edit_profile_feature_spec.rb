61e4257622892986b75ccf6fc61ba25241708455
Kolesnikov Alexander <alexander.kolesnikov@teknavo.com>
2015-07-08 17:48:15 +0300

    new test for password changing added

--- spec/acceptance_tests/account_management/edit_profile_feature_spec.rb
+++ spec/acceptance_tests/account_management/edit_profile_feature_spec.rb
@@ -1,4 +1,4 @@
-RSpec.feature 'user_management.user_login', :type => :feature do
+RSpec.feature 'user_management.edit_profile', :type => :feature do
   background do
     visit "/"
     Account.create(email: 'test1@user.com', password: '123456qwe', subdomain: 'MySuperSD')
@@ -13,7 +13,9 @@ RSpec.feature 'user_management.user_login', :type => :feature do
 
     scenario 'user visits edit profile page' do
       expect_page_url_to_be '/editprofile' 
-      
+      find_field('Password').visible?
+      find_field('Password confirmation').visible?
+      find_button("Change password", disabled: true)
     end
 
     scenario 'logged out user cannot visit edit profile page' do
@@ -24,18 +26,56 @@ RSpec.feature 'user_management.user_login', :type => :feature do
       expect_error('Log in to manage your store')
     end
 
-    scenario 'user changes password' do
-            
+    scenario 'user sets new password' do
+      fill_in('Password', with: '123456qwe_new')
+      fill_in('Password confirmation', with: '123456qwe_new')
+      click_button("Change password")
+      expect_page_url_to_be '/editprofile'
+      expect(page).to have_content('Password successfully changed')
     end
 
-  end
+    scenario 'user enters blank password' do
+      fill_in('Password', with: '')
+      fill_in('Password confirmation', with: '123456qwe_new')
+      find_button("Change password", disabled: true)
+    end
 
-  scenario 'dummy_scenario' do
-    visit '/'
-  end
+    scenario 'user re-enters blank password' do
+      fill_in('Password', with: '123456qwe_new')
+      fill_in('Password confirmation', with: '')
+      find_button("Change password", disabled: true)
+    end
 
+    scenario 'user re-enters incorrect password' do
+      fill_in('Password', with: '123456qwe_new')
+      fill_in('Password confirmation', with: '123456qwe_new123')
+      find_button("Change password", disabled: true)
+      expect_error('passwords do not match')
+    end
+
+    scenario 'user logs in with new password' do
+      fill_in('Password', with: '123456qwe_new')
+      fill_in('Password confirmation', with: '123456qwe_new')
+      click_button("Change password")
+      click_link('Log out')
+      log_in_with('test1@user.com','123456qwe_new')
+      expect_page_url_to_be '/admin'
+      expect(page).to have_content('Hello test1@user.com')
+      expect(page).to have_content('Subdomain MySuperSD')
+    end
+
+    scenario 'user tries to logs in with old password' do
+      fill_in('Password', with: '123456qwe_new')
+      fill_in('Password confirmation', with: '123456qwe_new')
+      click_button("Change password")
+      click_link('Log out')
+      log_in_with('test1@user.com','123456qwe')
+      expect_page_url_to_be '/'
+      expect_error('Incorrect email/password')
+    end
+  end
 end
 
-
-  
-
+#  scenario 'dummy_scenario' do
+#    visit '/'
+#  end
